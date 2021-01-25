05562e1e92b647f4f920d53632bac75cb325e3ee
byoungsoo <skwltg90@naver.com>
2021-01-25 22:01:55 +0900

    1

--- src/com/leetcode/easy/CountandSay.java
+++ src/com/leetcode/easy/CountandSay.java
@@ -11,23 +11,35 @@ public class CountandSay {
 	public String countAndSay(int n) {
 		String answer = "";
 		String result = "";
-		String digit = "";
+		char digit = ' ';
+		boolean flag = false;
 		int count = 0;
-		boolean change = false;
 
 		if (n == 1) {
 			return "1";
 		} else {
 			answer = countAndSay(n - 1);
 			for (int i = 0; i < answer.length(); i++) {
-				if (digit != answer.substring(i, i + 1)) {
-					result += String.valueOf(count) + digit;
-					digit = answer.substring(i, i + 1);
+				if (digit != answer.charAt(i)) {
+					if (i != 0) {
+						result += String.valueOf(count) + String.valueOf(digit);
+						count = 0;
+					}
+					digit = answer.charAt(i);
 				}
-				if (digit == answer.substring(i, i + 1)) {
+				if (digit == answer.charAt(i)) {
 					count++;
 				}
+
+				if (i == answer.length() - 1) {
+					flag = true;
+				}
 			}
+			if (flag = true) {
+				result += String.valueOf(count) + String.valueOf(digit);
+				count = 0;
+			}
+
 		}
 
 		return result;
