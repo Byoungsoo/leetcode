56ee5ada70036ab285abb5c6fe3807fb0cbd334e
unknown <byoungsoo@lgcns.com>
2021-02-08 16:47:38 +0900

    LongestSubstring

--- src/com/leetcode/medium/LongestSubstring.java
+++ src/com/leetcode/medium/LongestSubstring.java
@@ -7,9 +7,9 @@ public class LongestSubstring {
 
 	public static void main(String[] args) {
 		LongestSubstring ls = new LongestSubstring();
-		String s = "dabcfdf";
+		String s = "abba";
 
-		int answer = ls.lengthOfLongestSubstring1(s);
+		int answer = ls.lengthOfLongestSubstring(s);
 		System.out.println(answer);
 
 	}
@@ -20,38 +20,21 @@ public class LongestSubstring {
 			return 0;
 		}
 
-		int count = 0;
 		int sIndex = 0;
 		Map<Character, Integer> map = new HashMap<Character, Integer>();
 		int max = Integer.MIN_VALUE;
 
 		for (int i = 0; i < s.length(); i++) {
+
 			if (map.containsKey(s.charAt(i))) {
-				count = 0;
-				i = map.get(s.charAt(i));
-				map.clear();
-			} else {
-				map.put(s.charAt(i), i);
-				count++;
+				if (map.get(s.charAt(i)) > sIndex)
+					sIndex = map.get(s.charAt(i));
 			}
-			if (count > max) {
-				max = count;
+			if (i - sIndex + 1 > max) {
+				max = i - sIndex + 1;
 			}
+			map.put(s.charAt(i), i + 1);
 		}
 		return max;
-	}
-	public int lengthOfLongestSubstring1(String s) {
-		int startIndex = 0;
-		int maxLength = 0;
-		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-		for (int endIndex = 0; endIndex < s.length(); endIndex++) {
-			Character current = s.charAt(endIndex);
-			if (map.containsKey(current)) {
-				startIndex = Math.max(map.get(current), startIndex);
-			}
-			maxLength = Math.max(maxLength, endIndex - startIndex + 1);
-			map.put(current, endIndex + 1);
-		}
-		return maxLength;
 	}
 }
