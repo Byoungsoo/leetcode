27c601e8cd6082edaa4212c17066326de2426044
bys <skwltg90@naver.com>
2021-02-28 14:29:41 +0900

    LongestPalindromicSubstring

--- src/com/leetcode/medium/LongestPalindromicSubstring.java
+++ src/com/leetcode/medium/LongestPalindromicSubstring.java
@@ -4,39 +4,38 @@ public class LongestPalindromicSubstring {
 
 	public static void main(String[] args) {
 		LongestPalindromicSubstring lps = new LongestPalindromicSubstring();
-		String s = "babad";
+		String s = "bb";
 		String answer = lps.longestPalindrome(s);
 		System.out.println("answer: " + answer);
 
 	}
 
-	int result, maxLen;
+	int leftIndex, maxLen;
 
 	public String longestPalindrome(String s) {
 
 		int len = s.length();
 		if (len < 2)
 			return s;
-
+				
 		for (int i = 0; i < len - 1; i++) {
 			extendPalindrome(s, i, i);
 			extendPalindrome(s, i, i + 1);
 		}
-		return s.substring(result, result + maxLen);
+		return s.substring(leftIndex, leftIndex + maxLen);
 
 	}
 
 	public void extendPalindrome(String s, int left, int right) {
-		while (left >= 0 && right < s.length() - 1 && s.charAt(left) == s.charAt(right)) {
+		while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
 			left--;
 			right++;
 		}
 
 		if (maxLen < right - left - 1) {
-			result = left + 1;
+			leftIndex = left + 1;
 			maxLen = right - left - 1;
 		}
-
 	}
 
 }
