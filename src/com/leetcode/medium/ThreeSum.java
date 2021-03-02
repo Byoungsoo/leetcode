b51c924cfe25221a1ab84b1b50299245ff4e17b8
unknown <byoungsoo@lgcns.com>
2021-03-02 15:33:34 +0900

    3Sum

--- src/com/leetcode/medium/ThreeSum.java
+++ src/com/leetcode/medium/ThreeSum.java
@@ -1,15 +1,14 @@
 package com.leetcode.medium;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class ThreeSum {
 
 	public static void main(String[] args) {
 
-		int[] nums = { -1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4 };
+		int[] nums = { -1, -1, 0, 2 };
 		List<List<Integer>> list = new ArrayList<List<Integer>>();
 		ThreeSum ts = new ThreeSum();
 		list = ts.threeSum(nums);
@@ -26,18 +25,46 @@ public class ThreeSum {
 	public List<List<Integer>> threeSum(int[] nums) {
 
 		List<List<Integer>> answer = new ArrayList<List<Integer>>();
+		
 
-		for (int i = 0; i < nums.length; i++) {
-			for (int j = i + 1; j < nums.length; j++) {
-				for (int k = j + 1; k < nums.length; k++) {
-					if (nums[i] + nums[j] + nums[k] == 0) {
+		if (nums.length < 3) {
+			return answer;
+		}
 
-						List<Integer> list = new ArrayList<Integer>();
-						list.add(nums[i]);
-						list.add(nums[j]);
-						list.add(nums[k]);
-						answer.add(list);
-					}
+		Arrays.sort(nums);
+		int length = nums.length;
+		int sum;
+		int left;
+		int right;
+
+		for (int i = 0; i < length - 1; i++) {
+			left = i + 1;
+			right = length - 1;
+			if (i != 0 && nums[i - 1] == nums[i]) {
+				continue;
+			}
+			while (left < right) {
+				if(nums[i] > 0) return answer;
+				
+				sum = nums[i] + nums[left] + nums[right];
+				if (sum == 0) {
+					List<Integer> temp = new ArrayList<Integer>();
+					temp.add(nums[i]);
+					temp.add(nums[left]);
+					temp.add(nums[right]);
+					answer.add(temp);
+					left++;
+					while ((left < right) && (nums[left] == nums[left - 1]))
+						left++;// avoid duplicates
+
+					right--;
+					while ((left < right) && (nums[right] == nums[right + 1]))
+						right--;// avoid duplicates
+
+				} else if (sum < 0) {
+					left++;
+				} else if (sum > 0) {
+					right--;
 				}
 			}
 		}
